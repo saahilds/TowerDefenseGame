938fa078a3badc041f564e1aebb6f4b358b97a85
Adam Gerig <gerigadam2000@gmail.com>
2021-10-08 00:35:33 -0400

    GameSettingDataMap fixed

--- src/main/java/com/main/game/data/GameSettingDataMap.java
+++ src/main/java/com/main/game/data/GameSettingDataMap.java
@@ -6,25 +6,43 @@ import com.main.model.GameScreenType;
 import java.util.HashMap;
 
 public class GameSettingDataMap {
-    public static String welcomeFile = "main.fxml";
-    public static String configScreenFile = "initial-screen.fxml";
-    public static String gameScreenFile = "game-screen.fxml";
+    private static String welcomeFile = "main.fxml";
 
-    private static HashMap<GameScreenType, String> screenFileMap = new HashMap<>() {{
-        put(GameScreenType.WELCOME_SCREEN, welcomeFile);
-        put(GameScreenType.CONFIG_SCREEN, configScreenFile);
-        put(GameScreenType.GAME_SCREEN, gameScreenFile);
-    }};
+    public static String getWelcomeFile() {
+        return welcomeFile;
+    }
+
+    private static String configScreenFile = "initial-screen.fxml";
+
+    public static String getConfigScreenFile() {
+        return configScreenFile;
+    }
+
+    private static String gameScreenFile = "game-screen.fxml";
+
+    public static String getGameScreenFile() {
+        return gameScreenFile;
+    }
+
+    private static HashMap<GameScreenType, String> screenFileMap = new HashMap<>() {
+        {
+            put(GameScreenType.WELCOME_SCREEN, welcomeFile);
+            put(GameScreenType.CONFIG_SCREEN, configScreenFile);
+            put(GameScreenType.GAME_SCREEN, gameScreenFile);
+        }
+    };
 
     public static String getFileName(GameScreenType gameScreenType) {
         return screenFileMap.get(gameScreenType);
     }
 
-    private static HashMap<GameLevelType, Integer> startingMoneyMap  = new HashMap<>() {{
-        put(GameLevelType.EASY, 1000);
-        put(GameLevelType.NORMAL, 500);
-        put(GameLevelType.HARD, 100);
-    }};
+    private static HashMap<GameLevelType, Integer> startingMoneyMap  = new HashMap<>() {
+        {
+            put(GameLevelType.EASY, 1000);
+            put(GameLevelType.NORMAL, 500);
+            put(GameLevelType.HARD, 100);
+        }
+    };
 
     public static Integer getStartingMoney(GameLevelType gameLevelType) {
         return startingMoneyMap.get(gameLevelType);

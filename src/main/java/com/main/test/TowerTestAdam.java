02b44999acfcf14c753b5aaea218bc0570089f86
Adam Gerig <gerigadam2000@gmail.com>
2021-10-22 03:51:31 -0400

    Create TowerTestAdam.java

---
+++ src/main/java/com/main/test/TowerTestAdam.java
@@ -0,0 +1,39 @@
+package com.main.test;
+
+import com.main.MainApplication;
+import com.main.game.DataController;
+import com.main.game.data.GameSettingDataMap;
+import com.main.game.entity.EntityWithHealth;
+import com.main.model.GameLevelType;
+import com.main.model.GameScreenType;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.junit.Before;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+
+public class TowerTestAdam extends ApplicationTest {
+    private MainApplication app;
+    private DataController dataController;
+    private Parent mainNode;
+    private Stage mainstage;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        mainNode = (Parent) FXMLLoader.load(
+                MainApplication.class.getResource(
+                        GameSettingDataMap.getFileName(GameScreenType.GAME_SCREEN)
+                )
+        );
+        mainstage = stage;
+        stage.setScene(new Scene(mainNode));
+        stage.show();
+        stage.toFront();
+    }
+    
+}

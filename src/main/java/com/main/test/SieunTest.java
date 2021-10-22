6af6292c1b42228581a2b5acb9e5e7fda9da7e86
Sieun Kim <skim3256@gatech.edu>
2021-10-22 05:29:16 -0400

    fixed checkstyle

--- src/main/java/com/main/test/SieunTest.java
+++ src/main/java/com/main/test/SieunTest.java
@@ -3,33 +3,22 @@ package com.main.test;
 import com.main.MainApplication;
 import com.main.game.DataController;
 import com.main.game.data.GameSettingDataMap;
-import com.main.game.entity.tower.TowerData;
-import com.main.game.entity.tower.TowerMenu;
 import com.main.model.GameLevelType;
-import com.main.model.GameScreenType;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import org.junit.Before;
 import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
 
-import java.util.ArrayList;
-
 import static org.junit.Assert.*;
 
-public class tests extends ApplicationTest{
+public class SieunTest extends ApplicationTest{
     private MainApplication app;
     private DataController dataController;
     private Parent mainNode;
     private Stage mainstage;
 
     private int diff = 0;
-
-
 
     //private GameLevelType gameLevelType;
     @Override
@@ -52,7 +41,8 @@ public class tests extends ApplicationTest{
 
     @Test
     public void testCostForGameLevelEasy() {
-        if ((GameSettingDataMap.getTowerInitialCost(GameLevelType.EASY)) != GameSettingDataMap.getTowerInitialCost(GameLevelType.NORMAL)) {
+        if ((GameSettingDataMap.getTowerInitialCost(GameLevelType.EASY))
+                != GameSettingDataMap.getTowerInitialCost(GameLevelType.NORMAL)) {
             diff = 1;
         }
         assertSame(1, diff);
@@ -60,14 +50,16 @@ public class tests extends ApplicationTest{
 
     @Test
     public void testCostForGameLevelNormal() {
-        if ((GameSettingDataMap.getTowerInitialCost(GameLevelType.NORMAL)) != GameSettingDataMap.getTowerInitialCost(GameLevelType.HARD)) {
+        if ((GameSettingDataMap.getTowerInitialCost(GameLevelType.NORMAL))
+                != GameSettingDataMap.getTowerInitialCost(GameLevelType.HARD)) {
             diff = 1;
         }
         assertSame(1, diff);
     }
     @Test
     public void testCostForGameLevelHard() {
-        if ((GameSettingDataMap.getTowerInitialCost(GameLevelType.EASY)) != GameSettingDataMap.getTowerInitialCost(GameLevelType.HARD)) {
+        if ((GameSettingDataMap.getTowerInitialCost(GameLevelType.EASY))
+                != GameSettingDataMap.getTowerInitialCost(GameLevelType.HARD)) {
             diff = 1;
         }
         assertSame(1, diff);

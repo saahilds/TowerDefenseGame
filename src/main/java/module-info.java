dc2062364b1c6f8d7dcffaf81bd73c4a4a247626
jaeyoung-lee <jlee3414@gatech.edu>
2021-10-31 21:52:46 -0400

    edit dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,20 +1,10 @@
 module com.main {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
     requires junit;
     requires testfx.core;
     requires testfx.junit;
-    requires javafx.swing;
-    requires hamcrest.core;
-    //requires org.testfx.junit;
+    requires io.reactivex.rxjava2;
 
     opens com.main to javafx.fxml;
     exports com.main;

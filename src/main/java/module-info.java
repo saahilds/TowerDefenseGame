172de837fff58450a0c173e5043d4e21d710cf23
Saahil <ssharma438@gatech.edu>
2021-12-07 21:47:31 -0500

    Re comment module-info.java lines

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,9 @@ module com.main {
     requires javafx.controls;
     requires javafx.fxml;
     requires junit;
-    //requires testfx.core;
-    //requires testfx.junit;
-    //requires io.reactivex.rxjava2;
+    requires testfx.core;
+    requires testfx.junit;
+    requires io.reactivex.rxjava2;
     requires org.kordamp.bootstrapfx.core;
     requires rxjavafx;
     requires hamcrest.core;
